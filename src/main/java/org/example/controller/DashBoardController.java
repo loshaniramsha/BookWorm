20146ff7ec9bb07219cc7299d503ac45336a1b06
loshaniramsha <loshanikottagoda@gmail.com>
2024-02-28 02:56:36 +0530

    designed ui

---
+++ src/main/java/org/example/controller/DashBoardController.java
@@ -0,0 +1,51 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class DashBoardController {
+    public AnchorPane root;
+    public AnchorPane child;
+    public Button bt1;
+    public Button bt2;
+    public Button bt3;
+    public Button bt4;
+    public Button bt5;
+    public Label contact;
+    public ComboBox cmb;
+    public Label link;
+
+    public void searchinOnAction(ActionEvent actionEvent) {
+    }
+
+    public void dashboardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void userOnAction(ActionEvent actionEvent) {
+    }
+
+    public void BookOnAction(ActionEvent actionEvent) {
+    }
+
+    public void borrowOnAction(ActionEvent actionEvent) {
+    }
+
+    public void branchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void lblExits(MouseEvent mouseEvent) {
+    }
+
+    public void lblContactOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void cmbOnAction(ActionEvent actionEvent) {
+    }
+
+    public void lblClick(MouseEvent mouseEvent) {
+    }
+}
---
+++ src/main/java/org/example/controller/LoginFormController.java
@@ -0,0 +1,18 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public AnchorPane root;
+    public TextField textName;
+    public TextField textPassword;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void lblcreateAccount(MouseEvent mouseEvent) {
+    }
+}
