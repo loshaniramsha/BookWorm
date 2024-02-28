a478e9b566c225760badf223b04e5c44054db002
loshaniramsha <loshanikottagoda@gmail.com>
2024-02-28 13:46:30 +0530

    changes

--- src/main/java/org/example/AppInitializer.java
+++ src/main/java/org/example/AppInitializer.java
@@ -1,4 +1,23 @@
 package org.example;
 
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent load = FXMLLoader.load(this.getClass().getResource("/view/Login_Form.fxml"));
+        Scene scene=new Scene(load);
+        stage.setScene(scene);
+        stage.setTitle("Login Form");
+        stage.centerOnScreen();
+        stage.show();
+    }
 }
